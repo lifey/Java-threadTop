de8df7674b639569b71ca0f5bbf4df499d260100
Haim Yadid <haim@performize-it.com>
2014-03-13 15:25:04 +0200

    fix pom

--- src/main/scripts/threadTop.bat
+++ src/main/scripts/threadTop.bat
@@ -1,1 +1,3 @@
-java -cp "%JAVA_HOME%/lib/tools.jar";threadTop-1.1.jar com.performizeit.threadtop.Main %*
+@echo off
+set d=%~dp0
+java -cp "%JAVA_HOME%/lib/tools.jar";%d%/threadTop-1.1.jar com.performizeit.threadtop.Main %*

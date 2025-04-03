fc26135a0d88889c6f137e511e081624b78fa34e
lifey <haim@next-insurance.com>
2025-04-03 13:22:47 +0300

    fix attach warning

--- src/main/scripts/threadTop.sh
+++ src/main/scripts/threadTop.sh
@@ -9,9 +9,13 @@ fi
 
 # --add-exports and --add-opens flags are needed to allow JDK modules access for the JMX attach API
 java --add-exports java.base/jdk.internal.perf=ALL-UNNAMED \
-     --add-exports jdk.management/sun.management=ALL-UNNAMED \
+     --add-exports jdk.management.agent/jdk.internal.agent=ALL-UNNAMED \
+     --add-exports java.management/sun.management=ALL-UNNAMED \
+     --add-exports jdk.attach/sun.tools.attach=ALL-UNNAMED \
      --add-opens java.base/java.util=ALL-UNNAMED \
      --add-opens java.management/javax.management.remote=ALL-UNNAMED \
+     --add-opens jdk.management/com.sun.management.internal=ALL-UNNAMED \
      --add-modules jdk.attach,jdk.management.agent \
      -Djdk.attach.allowAttachSelf=true \
+     -XX:+IgnoreUnrecognizedVMOptions \
      -jar "${SCRIPT_DIR}/threadTop-1.1.jar" "$@"
